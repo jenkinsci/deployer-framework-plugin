ac84ee09b3232972a200d07b562eec540edd8137
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2020-07-03 12:31:52 +0200

    Simplify test code for jcasc support

--- src/test/java/com/cloudbees/plugins/deployer/casc/ConfigurationAsCodeTest.java
+++ src/test/java/com/cloudbees/plugins/deployer/casc/ConfigurationAsCodeTest.java
@@ -4,11 +4,11 @@ import com.cloudbees.plugins.deployer.DeployNowColumn;
 import hudson.model.View;
 import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.stream.StreamSupport;
 
 public class ConfigurationAsCodeTest {
     @Rule
@@ -20,8 +20,11 @@ public class ConfigurationAsCodeTest {
         final View view = r.jenkins.getView("view-with-deployNowColumn");
         Assert.assertNotNull(view);
 
-        MatcherAssert.assertThat(view.getColumns(),
-              Matchers.hasItem(Matchers.instanceOf(DeployNowColumn.class))
+        Assert.assertTrue(
+            StreamSupport.stream(view.getColumns().spliterator(), false)
+                  .anyMatch(
+                        item -> item instanceof DeployNowColumn
+                  )
         );
     }
 }
